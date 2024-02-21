823c71c0fb73f7704d72e060b17d3047f4a7110c
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-21 20:19:48 +0200

    Initial Commit

---
+++ HowToUseGridInSwiftUI/HowToUseGridInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseGridInSwiftUIApp.swift
+//  HowToUseGridInSwiftUI
+//
+//  Created by test on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseGridInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
